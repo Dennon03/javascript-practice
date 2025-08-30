7c2c311ab6f28c082ee81635b76c729399b543f5
Dennon Parks <parksdennon@gmail.com>
2025-08-29 20:24:30 -0600

    save data and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -3,7 +3,8 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '1'
   }];
 }
 
@@ -25,7 +26,8 @@ export function addToCart (productId) {
   } else {
       cart.push({
       productId: productId,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '1'
     });
   }
 
